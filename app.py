eb2f653dac2cab25680b11f4e0eb82bffa1a854e
Sanderson Rhawan da Silva Torres <106162224+Darknios@users.noreply.github.com>
2026-06-21 23:43:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import streamlit as st
 
 BASE_DIR = Path(__file__).parent
 ASSETS_DIR = BASE_DIR / "assets"
-MANUAL_JSON = DATA_DIR / "manual_credenciape_paginas.json"
+MANUAL_JSON = "manual_credenciape_paginas.json"
 MANUAL_PDF = DATA_DIR / "manual_credenciape.pdf"
 ENV_FILE = BASE_DIR / ".env"
 
