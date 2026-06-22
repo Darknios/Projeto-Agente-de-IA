d2e2d5e67c96f10358df66ede5adff89855b1dac
Sanderson Rhawan da Silva Torres <106162224+Darknios@users.noreply.github.com>
2026-06-22 14:37:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,11 +10,9 @@ from typing import Dict, List, Tuple
 
 import streamlit as st
 
-BASE_DIR = Path(__file__).parent
-ASSETS_DIR = BASE_DIR / "assets"
-MANUAL_JSON = "manual_credenciape_paginas.json"
-MANUAL_PDF = "manual_credenciape.pdf"
-ENV_FILE = ".env"
+MANUAL_JSON = BASE_DIR / "manual_credenciape_paginas.json"
+MANUAL_PDF = BASE_DIR / "manual_credenciape.pdf"
+ENV_FILE = BASE_DIR / ".env"
 
 AZUL_ESCURO = "#173B8F"
 AZUL = "#235EE8"
